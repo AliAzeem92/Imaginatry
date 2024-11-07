dc72618f45c192af034c7908110c5c05395e2fbf
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-11-07 18:45:08 +0500

    second commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,3 +1,39 @@
+// import mongoose, { Mongoose } from "mongoose";
+
+// const MONGODB_URL = process.env.MONGODB_URL;
+
+// interface MongooseConnection {
+//   conn: Mongoose | null;
+//   promise: Promise<Mongoose> | null;
+// }
+
+// let cached: MongooseConnection = (global as any).mongoose;
+
+// if (!cached) {
+//   cached = (global as any).mongoose = {
+//     conn: null,
+//     promise: null,
+//   };
+// }
+
+// export const connectToDatabase = async () => {
+//   if (cached.conn) return cached.conn;
+
+//   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
+//   cached.promise =
+//     cached.promise ||
+//     mongoose.connect(MONGODB_URL, {
+//       dbName: "imaginatry",
+//       bufferCommands: false,
+//     });
+
+//   cached.conn = await cached.promise;
+
+//   return cached.conn;
+// };
+//
+//
 import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
@@ -7,16 +43,21 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseConnection | undefined;
 }
 
-export const connectToDatabase = async () => {
+const cached: MongooseConnection = global.mongoose || {
+  conn: null,
+  promise: null,
+};
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+export const connectToDatabase = async (): Promise<Mongoose> => {
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,34 +1,191 @@
+// /* eslint-disable prefer-const */
+// /* eslint-disable no-prototype-builtins */
+// import { type ClassValue, clsx } from "clsx";
+// import qs from "qs";
+// import { twMerge } from "tailwind-merge";
+
+// import { aspectRatioOptions } from "@/constants";
+
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs));
+// }
+
+// // ERROR HANDLER
+// export const handleError = (error: unknown) => {
+//   if (error instanceof Error) {
+//     // This is a native JavaScript error (e.g., TypeError, RangeError)
+//     console.error(error.message);
+//     throw new Error(`Error: ${error.message}`);
+//   } else if (typeof error === "string") {
+//     // This is a string error message
+//     console.error(error);
+//     throw new Error(`Error: ${error}`);
+//   } else {
+//     // This is an unknown type of error
+//     console.error(error);
+//     throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+//   }
+// };
+
+// // PLACEHOLDER LOADER - while image is transforming
+// const shimmer = (w: number, h: number) => `
+// <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+//   <defs>
+//     <linearGradient id="g">
+//       <stop stop-color="#7986AC" offset="20%" />
+//       <stop stop-color="#68769e" offset="50%" />
+//       <stop stop-color="#7986AC" offset="70%" />
+//     </linearGradient>
+//   </defs>
+//   <rect width="${w}" height="${h}" fill="#7986AC" />
+//   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+//   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+// </svg>`;
+
+// const toBase64 = (str: string) =>
+//   typeof window === "undefined"
+//     ? Buffer.from(str).toString("base64")
+//     : window.btoa(str);
+
+// export const dataUrl = `data:image/svg+xml;base64,${toBase64(
+//   shimmer(1000, 1000)
+// )}`;
+// // ==== End
+
+// // FORM URL QUERY
+// export const formUrlQuery = ({
+//   searchParams,
+//   key,
+//   value,
+// }: FormUrlQueryParams) => {
+//   const params = { ...qs.parse(searchParams.toString()), [key]: value };
+
+//   return `${window.location.pathname}?${qs.stringify(params, {
+//     skipNulls: true,
+//   })}`;
+// };
+
+// // REMOVE KEY FROM QUERY
+// export function removeKeysFromQuery({
+//   searchParams,
+//   keysToRemove,
+// }: RemoveUrlQueryParams) {
+//   const currentUrl = qs.parse(searchParams);
+
+//   keysToRemove.forEach((key) => {
+//     delete currentUrl[key];
+//   });
+
+//   // Remove null or undefined values
+//   Object.keys(currentUrl).forEach(
+//     (key) => currentUrl[key] == null && delete currentUrl[key]
+//   );
+
+//   return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+// }
+
+// // DEBOUNCE
+// export const debounce = (func: (...args: any[]) => void, delay: number) => {
+//   let timeoutId: NodeJS.Timeout | null;
+//   return (...args: any[]) => {
+//     if (timeoutId) clearTimeout(timeoutId);
+//     timeoutId = setTimeout(() => func.apply(null, args), delay);
+//   };
+// };
+
+// // GE IMAGE SIZE
+// export type AspectRatioKey = keyof typeof aspectRatioOptions;
+// export const getImageSize = (
+//   type: string,
+//   image: any,
+//   dimension: "width" | "height"
+// ): number => {
+//   if (type === "fill") {
+//     return (
+//       aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+//       1000
+//     );
+//   }
+//   return image?.[dimension] || 1000;
+// };
+
+// // DOWNLOAD IMAGE
+// export const download = (url: string, filename: string) => {
+//   if (!url) {
+//     throw new Error("Resource URL not provided! You need to provide one");
+//   }
+
+//   fetch(url)
+//     .then((response) => response.blob())
+//     .then((blob) => {
+//       const blobURL = URL.createObjectURL(blob);
+//       const a = document.createElement("a");
+//       a.href = blobURL;
+
+//       if (filename && filename.length)
+//         a.download = `${filename.replace(" ", "_")}.png`;
+//       document.body.appendChild(a);
+//       a.click();
+//     })
+//     .catch((error) => console.log({ error }));
+// };
+
+// // DEEP MERGE OBJECTS
+// export const deepMergeObjects = (obj1: any, obj2: any) => {
+//   if (obj2 === null || obj2 === undefined) {
+//     return obj1;
+//   }
+
+//   let output = { ...obj2 };
+
+//   for (let key in obj1) {
+//     if (obj1.hasOwnProperty(key)) {
+//       if (
+//         obj1[key] &&
+//         typeof obj1[key] === "object" &&
+//         obj2[key] &&
+//         typeof obj2[key] === "object"
+//       ) {
+//         output[key] = deepMergeObjects(obj1[key], obj2[key]);
+//       } else {
+//         output[key] = obj1[key];
+//       }
+//     }
+//   }
+
+//   return output;
+// };
+//
+//
+//
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
-
 import { aspectRatioOptions } from "@/constants";
 
+// Function to merge class names with Tailwind CSS
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 // ERROR HANDLER
-export const handleError = (error: unknown) => {
+export const handleError = (error: unknown): void => {
   if (error instanceof Error) {
-    // This is a native JavaScript error (e.g., TypeError, RangeError)
     console.error(error.message);
     throw new Error(`Error: ${error.message}`);
   } else if (typeof error === "string") {
-    // This is a string error message
     console.error(error);
     throw new Error(`Error: ${error}`);
   } else {
-    // This is an unknown type of error
     console.error(error);
     throw new Error(`Unknown error: ${JSON.stringify(error)}`);
   }
 };
 
 // PLACEHOLDER LOADER - while image is transforming
-const shimmer = (w: number, h: number) => `
+const shimmer = (w: number, h: number): string => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
@@ -39,10 +196,11 @@ const shimmer = (w: number, h: number) => `
   </defs>
   <rect width="${w}" height="${h}" fill="#7986AC" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite" />
 </svg>`;
 
-const toBase64 = (str: string) =>
+// Convert a string to base64 format (for server-side)
+const toBase64 = (str: string): string =>
   typeof window === "undefined"
     ? Buffer.from(str).toString("base64")
     : window.btoa(str);
@@ -50,14 +208,19 @@ const toBase64 = (str: string) =>
 export const dataUrl = `data:image/svg+xml;base64,${toBase64(
   shimmer(1000, 1000)
 )}`;
-// ==== End
 
 // FORM URL QUERY
+interface FormUrlQueryParams {
+  searchParams: URLSearchParams;
+  key: string;
+  value: string;
+}
+
 export const formUrlQuery = ({
   searchParams,
   key,
   value,
-}: FormUrlQueryParams) => {
+}: FormUrlQueryParams): string => {
   const params = { ...qs.parse(searchParams.toString()), [key]: value };
 
   return `${window.location.pathname}?${qs.stringify(params, {
@@ -66,17 +229,21 @@ export const formUrlQuery = ({
 };
 
 // REMOVE KEY FROM QUERY
+interface RemoveUrlQueryParams {
+  searchParams: URLSearchParams;
+  keysToRemove: string[];
+}
+
 export function removeKeysFromQuery({
   searchParams,
   keysToRemove,
-}: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(searchParams);
+}: RemoveUrlQueryParams): string {
+  const currentUrl = qs.parse(searchParams.toString());
 
   keysToRemove.forEach((key) => {
     delete currentUrl[key];
   });
 
-  // Remove null or undefined values
   Object.keys(currentUrl).forEach(
     (key) => currentUrl[key] == null && delete currentUrl[key]
   );
@@ -85,19 +252,23 @@ export function removeKeysFromQuery({
 }
 
 // DEBOUNCE
-export const debounce = (func: (...args: any[]) => void, delay: number) => {
+export const debounce = (
+  func: (...args: unknown[]) => void,
+  delay: number
+): ((...args: unknown[]) => void) => {
   let timeoutId: NodeJS.Timeout | null;
-  return (...args: any[]) => {
+  return (...args: unknown[]): void => {
     if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
+    timeoutId = setTimeout(() => func(...args), delay);
   };
 };
 
-// GE IMAGE SIZE
+// GET IMAGE SIZE
 export type AspectRatioKey = keyof typeof aspectRatioOptions;
+
 export const getImageSize = (
   type: string,
-  image: any,
+  image: Record<string, unknown>,
   dimension: "width" | "height"
 ): number => {
   if (type === "fill") {
@@ -106,11 +277,13 @@ export const getImageSize = (
       1000
     );
   }
-  return image?.[dimension] || 1000;
+
+  const dimensionValue = image?.[dimension];
+  return typeof dimensionValue === "number" ? dimensionValue : 1000;
 };
 
 // DOWNLOAD IMAGE
-export const download = (url: string, filename: string) => {
+export const download = (url: string, filename: string): void => {
   if (!url) {
     throw new Error("Resource URL not provided! You need to provide one");
   }
@@ -127,11 +300,16 @@ export const download = (url: string, filename: string) => {
       document.body.appendChild(a);
       a.click();
     })
-    .catch((error) => console.log({ error }));
+    .catch((error) => {
+      console.error("Download error:", error);
+    });
 };
 
 // DEEP MERGE OBJECTS
-export const deepMergeObjects = (obj1: any, obj2: any) => {
+export const deepMergeObjects = (
+  obj1: Record<string, unknown>,
+  obj2: Record<string, unknown>
+): Record<string, unknown> => {
   if (obj2 === null || obj2 === undefined) {
     return obj1;
   }
@@ -146,7 +324,10 @@ export const deepMergeObjects = (obj1: any, obj2: any) => {
         obj2[key] &&
         typeof obj2[key] === "object"
       ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+        output[key] = deepMergeObjects(
+          obj1[key] as Record<string, unknown>,
+          obj2[key] as Record<string, unknown>
+        );
       } else {
         output[key] = obj1[key];
       }
